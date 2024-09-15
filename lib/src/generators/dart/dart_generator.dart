fcb8998fb95a5d0169d5ca9a99aff011d3d81abb
andikadeveloper <mandikadev@gmail.com>
2024-09-15 21:50:25 +0700

    feat: add ability to generate CRUD code

--- lib/src/generators/dart/dart_generator.dart
+++ lib/src/generators/dart/dart_generator.dart
@@ -1,2 +1,3 @@
 export 'package:supagen/src/generators/dart/flutter_project_generator.dart';
 export 'package:supagen/src/generators/dart/dart_model_generator.dart';
+export 'package:supagen/src/generators/dart/dart_repository_generator.dart';
---
+++ lib/src/generators/dart/dart_repository_generator.dart
@@ -0,0 +1,45 @@
+import 'dart:io';
+
+import 'package:mason/mason.dart';
+import 'package:supagen/src/utils/extensions/extensions.dart';
+
+class DartRepositoryGenerator {
+  final Logger logger;
+
+  DartRepositoryGenerator({
+    required this.logger,
+  });
+
+  Future<void> generate({
+    required String projectName,
+    required Map<String, dynamic> tableDefinitions,
+  }) async {
+    final path = './$projectName/lib/repositories';
+    logger.info('Generating Dart repositories in: $path...');
+
+    final target = DirectoryGeneratorTarget(
+      Directory.fromUri(Uri.parse(path)),
+    );
+
+    logger.info('Fetching Dart repository template...');
+    final brick = Brick.git(
+      const GitPath(
+        'https://github.com/supagen/supagen_bricks.git',
+        path: 'bricks/dart/repository',
+      ),
+    );
+    final generator = await MasonGenerator.fromBrick(brick);
+    logger.info('Dart repository template fetched successfully!');
+
+    for (final table in tableDefinitions.keys) {
+      logger.info('Generating Dart repository for table: $table...');
+      await generator.generate(target, vars: <String, dynamic>{
+        'model_name': table.singularize,
+        'table_name': table,
+      });
+      logger.info('Dart repository generated successfully for table: $table');
+    }
+
+    logger.info('Dart repositories generated successfully in: $path');
+  }
+}
