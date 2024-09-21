83f76386a1a2830695312dd5fac19f95e3c5a1f1
andikadeveloper <mandikadev@gmail.com>
2024-09-21 18:18:16 +0700

    ci: update github workflow to use grinder

--- Makefile
+++ Makefile
@@ -2,3 +2,8 @@ run:
 	dart run bin/main.dart
 compile:
 	dart compile exe bin/main.dart -o supagen
+release:
+	git tag -a $(version) -m "Release $(version)"
+	git push origin $(version)
+changelog:
+	git log --pretty=format:"%s" $(shell git describe --tags --abbrev=0)..HEAD > releases.txt
---
+++ tool/grind.dart
@@ -0,0 +1,29 @@
+import 'dart:io';
+
+import 'package:cli_pkg/cli_pkg.dart' as pkg;
+import 'package:grinder/grinder.dart';
+import 'package:supagen/src/utils/constants.dart';
+
+const _packageName = kPackageName;
+const owner = kPackageName;
+const repo = kPackageName;
+
+void main(List<String> args) {
+  pkg.name.value = _packageName;
+  pkg.humanName.value = _packageName;
+  pkg.githubUser.value = owner;
+  pkg.githubRepo.value = '$owner/$repo';
+  pkg.githubBearerToken.value = Platform.environment['GITHUB_TOKEN'];
+
+  pkg.addGithubTasks();
+
+  grind(args);
+}
+
+@Task('Compile')
+void compile() {
+  run(
+    'dart',
+    arguments: ['compile', 'exe', 'bin/main.dart', '-o', _packageName],
+  );
+}
